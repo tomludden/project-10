d515fb6e90758db1df07f830d464562f4a8f5875
tomludden <148908140+tomludden@users.noreply.github.com>
2025-06-16 23:42:48 +0200

    fix

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// api/index.js
-const serverlessExpress = require('@vendia/serverless-express')
-const app = require('../app') // relative path to app.js
-
-module.exports = serverlessExpress({ app })
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-// app.js
 require('dotenv').config()
 const express = require('express')
 const { connectDB } = require('./src/config/db.js')
 const eventsRouter = require('./src/api/routes/events.js')
 const usersRouter = require('./src/api/routes/users.js')
 const cors = require('cors')
-
 const app = express()
 
 connectDB()
 
 app.use(cors())
+
 app.use(express.json())
 
 app.use('/api/v1/events', eventsRouter)
 app.use('/api/v1/users', usersRouter)
 
-module.exports = app
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
